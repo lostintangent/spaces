364b7bb2734d5b0f3c20e7246f8193a7fe59bdf1
Arjun Attam <arjunattam@gmail.com>
2019-07-29 19:38:00 -0700

    Update connecting logic in ws

--- src/ws.ts
+++ src/ws.ts
@@ -3,52 +3,42 @@ import * as WebSocket from "ws";
 const BASE_URL = "http://vslscommunitieswebapp.azurewebsites.net";
 
 export class WebsocketClient {
-  wsUrl: string;
-  ws: WebSocket | undefined;
-  isAlive: boolean = false;
-  pingTimer: NodeJS.Timer | undefined;
+    wsUrl: string;
+    ws: WebSocket | undefined;
+    pingTimer: NodeJS.Timer | undefined;
 
-  constructor(userEmail: string, private callback: any) {
-    this.wsUrl = `${BASE_URL}/ws?${userEmail}`;
-  }
-
-  init() {
-    console.log("Websocket connecting");
-    this.ws = new WebSocket(this.wsUrl);
-    this.ws.on("open", () => this.receivedHeartbeat())
-    this.ws.on("pong", () => this.receivedHeartbeat())
-    this.ws.on("close", (code, reason) => {
-      console.log("Websocket closed")
-      this.initiateReconnection();
-    });
-
-    this.pingTimer = setInterval(() => {
-      if (!this.isAlive) {
-        console.log("Websocket disconnected");
-        this.initiateReconnection();
-      }
-
-      this.isAlive = false;
-      this.ws!.send("ping"); // We are sending a ping as a message
-    }, 5000)
-
-    this.ws.on("message", (data: any) => {
-      this.callback(JSON.parse(data));
-      this.receivedHeartbeat();
-    })
-  }
-
-  receivedHeartbeat() {
-    this.isAlive = true;
-  }
-
-  initiateReconnection() {
-    if (this.pingTimer) {
-      clearInterval(this.pingTimer)
+    constructor(userEmail: string, private callback: any) {
+        this.wsUrl = `${BASE_URL}/ws?${userEmail}`;
     }
 
-    setTimeout(() => {
-      this.init()
-    }, 5000)
-  }
+    init() {
+        console.log("Websocket connecting");
+        this.ws = new WebSocket(this.wsUrl);
+
+        this.ws.on("open", () => {
+            console.log("Websocket open");
+            this.pingTimer = setInterval(() => {
+                this.ws!.send("ping");
+            }, 7500)
+        })
+
+        this.ws.on("close", (code, reason) => {
+            console.log("Websocket closed")
+            this.initiateReconnection();
+        });
+
+        this.ws.on("message", (data: any) => {
+            this.callback(JSON.parse(data));
+        })
+    }
+
+    initiateReconnection() {
+        if (this.pingTimer) {
+            clearInterval(this.pingTimer)
+        }
+
+        setTimeout(() => {
+            this.init()
+        }, 5000)
+    }
 }

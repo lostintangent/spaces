da5a858616a2a098dc322947e9f885bed650f477
Oleg Solomka <legomushroom@gmail.com>
2019-08-14 12:24:33 -0700

    [auth][ex]: more refactoring

--- service/lib/communities/auth.ex
+++ service/lib/communities/auth.ex
@@ -3,27 +3,31 @@ defmodule LiveShareCommunities.Authentication do
 
   def init(opts), do: opts
 
-  def findPublicKey?({:ok, kid, token}) do
-    # TODO: add the logic to cache cert keys
+
+  def getKeys() do
     response = HTTPotion.get "https://login.microsoftonline.com/common/discovery/v2.0/keys"
     jsonBody = Poison.decode!(response.body)
-    keys = jsonBody["keys"]
+    jsonBody["keys"]
+  end
 
-    certItem = Enum.find(keys, fn key ->
-      key["kid"] == kid
-    end)
+  def findPublicKey?(arg) do
+    case arg do
+      {:ok, kid, token} ->
+        keys = getKeys()
+        certItem = Enum.find(keys, fn key ->
+          key["kid"] == kid
+        end)
 
-    if certItem == nil do
-      ""
+        certKey = Enum.at(certItem["x5c"], 0)
+
+        if certKey == "" do
+          {:error, "Cannot find appropriate public key."}
+        else
+          {:ok, certKey, token}
+        end
+      {:error, reason} -> {:error, reason}
     end
 
-    certKey = Enum.at(certItem["x5c"], 0)
-
-    if certKey == "" do
-      raise "Cannot find appropriate public key."
-    end
-
-    {:ok, certKey, token}
   end
 
   def validIssuer?({:ok, claims}) do
@@ -58,91 +62,102 @@ defmodule LiveShareCommunities.Authentication do
     end
   end
 
-  def getKid?({:ok, token}) do
-    try do
-      protected = JOSE.JWT.peek_protected(token)
-      kid = protected.fields["kid"]
-      {:ok, kid, token}
-    rescue
-      # :exit, _ -> "exit blocked"
-      e in ArgumentError -> e
-      {:error, "Failed to parse kid."}
-    end
-  end
-
-  def createCert?({:ok, certKey, token}) do
-    cert = "-----BEGIN CERTIFICATE-----\n#{certKey}\n-----END CERTIFICATE-----"
-    try do
-      jwk = JOSE.JWK.from_pem(cert)
-      {:ok, jwk, token}
-    rescue
-      e in ArgumentError -> e
-      {:error, "Failed to create cert."}
-    end
-  end
-
-  def verifyToken?({:ok, jwk, token}) do
-    case JOSE.JWT.verify_strict(jwk, ["RS256"], token) do
-      {ok, claims, _} ->
-        if ok == false do
-          {:error, "Token is not valid"}
-        else
-          {:ok, claims}
-            |> validIssuer?
-            |> validAudience?
-            |> validExpiration?
+  def getKid?(arg) do
+    case arg do
+      {:ok, token} ->
+        try do
+          protected = JOSE.JWT.peek_protected(token)
+          kid = protected.fields["kid"]
+          {:ok, kid, token}
+        rescue
+          e in ArgumentError ->
+            {:error, "Failed to parse kid."}
         end
-      {:error, error} ->
-        IO.inspect error
-        {:error, error}
+      {:error, reason} -> {:error, reason}
     end
   end
 
-  def isValidAADToken(token) do
-    try do
-      {:ok, jwk} = {:ok, token}
-        |> getKid?
-        |> findPublicKey?
-        |> createCert?
-        |> verifyToken?
-    rescue
-      e in FunctionClauseError ->
-        {:error, "Token is invalid. #{e.function}"}
+  def createCert?(arg) do
+    case arg do
+      {:ok, certKey, token} ->
+        cert = "-----BEGIN CERTIFICATE-----\n#{certKey}\n-----END CERTIFICATE-----"
+        try do
+          jwk = JOSE.JWK.from_pem(cert)
+          {:ok, jwk, token}
+        rescue
+          e in ArgumentError ->
+            {:error, "Failed to create cert."}
+        end
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def verifyToken?(arg) do
+    case arg do
+      {:ok, jwk, token} ->
+        case JOSE.JWT.verify_strict(jwk, ["RS256"], token) do
+          {ok, claims, _} ->
+            if ok == false do
+              {:error, "Token is not valid"}
+            else
+              {:ok, claims}
+                |> validIssuer?
+                |> validAudience?
+                |> validExpiration?
+            end
+          {:error, error} ->
+            {:error, error}
+        end
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def isValidAADToken?(arg) do
+    arg
+      |> getKid?
+      |> findPublicKey?
+      |> createCert?
+      |> verifyToken?
+  end
+
+  def isAuthHeaderPresent?(arg) do
+    case arg do
+      {:ok, conn} ->
+        auth_header_list = conn |> get_req_header("authorization")
+        len = auth_header_list |> length
+        if len < 1 do
+          {:error, "No authorization header set."}
+        else
+          auth_header = Enum.at(auth_header_list, 0)
+          values = String.split(auth_header, " ")
+
+          type = Enum.at(values, 0)
+          token = Enum.at(values, 1)
+          if token == nil do
+            {:error, "No authorization token found."}
+          else
+            {:ok, token}
+          end
+        end
+      {:error, reason} -> {:error, reason}
     end
   end
 
   def authenticated?(conn) do
-    auth_header_list = conn |> get_req_header("authorization")
-    len = auth_header_list |> length
-
-    # no auth header
-    if len < 1 do
-      false
-    end
-
-    auth_header = Enum.at(auth_header_list, 0)
-
-    values = String.split(auth_header, " ")
-
-    type = Enum.at(values, 0)
-    token = Enum.at(values, 1)
-
-    case isValidAADToken(token) do
-      {:ok, claims} ->
-        true
-      {:error, reason} ->
-        IO.inspect reason
-        false
-    end
+    {:ok, conn}
+      |> isAuthHeaderPresent?
+      |> isValidAADToken?
   end
 
   def call(conn, _opts) do
-    if authenticated?(conn) do
-      conn
-    else
-      conn
-      |> send_resp(401, "")
-      |> halt
+    case authenticated?(conn) do
+      {:ok, claims} ->
+        conn
+      {:error, reason} ->
+        IO.inspect reason
+        conn
+          |> send_resp(401, "")
+          |> halt
     end
   end
 end

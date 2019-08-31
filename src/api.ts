bbe20575370a7f77c6e30c552f3e8f5db765c4cd
Jonathan Carter <jc@trl.mx>
2019-08-31 12:54:57 -0600

    Fixing service URL

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { auth } from "./auth/auth";
+import { SERVICE_URL } from "./constants";
 import { ICommunity } from "./store/model";
 
-//const BASE_URL = `${SERVICE_URL}/v0`;
-const BASE_URL = "http://localhost:4000/v0";
+const BASE_URL = `${SERVICE_URL}/v0`;
+//const BASE_URL = "http://localhost:4000/v0";
 
 const getAuthHeader = async () => {
   const t1 = Date.now();

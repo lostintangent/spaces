8a0680d9396e2fe2592b8f636e3729c75523e04e
Arjun Attam <arjunattam@gmail.com>
2019-09-16 15:26:14 -0700

    Removing vscode-account for demo

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,25 +1,24 @@
-import * as vscode from 'vscode';
-import * as vscodeAccountAPI from '@vs/vscode-account';
-import { getKeytar } from './getKeytar';
+import * as vscode from "vscode";
 
 export class Authentication {
-    private authAPI?: vscodeAccountAPI.VSCodeAccount;
+  // private authAPI?: vscodeAccountAPI.VSCodeAccount;
 
-    public async init(context: vscode.ExtensionContext, strategies: vscodeAccountAPI.IAuthStrategy[] = []) {
-        this.authAPI = await (vscodeAccountAPI as any).activateInternal(
-            context,
-            [ ...strategies ],
-            getKeytar(),
-        );
-    }
+  public async init(context: vscode.ExtensionContext, strategies: any = []) {
+    // this.authAPI = await (vscodeAccountAPI as any).activateInternal(
+    //     context,
+    //     [ ...strategies ],
+    //     getKeytar(),
+    // );
+  }
 
-    public get API(): vscodeAccountAPI.VSCodeAccount {
-        if (!this.authAPI) {
-            throw new Error('The auth API wasn\'t yet initialized. Please call the `init` function first');
-        }
+  public get API() {
+    // if (!this.authAPI) {
+    //     throw new Error('The auth API wasn\'t yet initialized. Please call the `init` function first');
+    // }
 
-        return this.authAPI;
-    }
+    // return this.authAPI;
+    return null;
+  }
 }
 
 export const auth = new Authentication();

11dd91666d5fa539c4fccdc3e1450a64158f036c
Jonathan Carter <joncart@microsoft.com>
2019-10-01 11:46:22 -0700

    Fixing join requests

--- src/contacts/messaging/ContactMessageManager.ts
+++ src/contacts/messaging/ContactMessageManager.ts
@@ -36,11 +36,14 @@ export class ContactMessageManager {
   private initializeProvider(presenceProvider: any) {
     this.presenceProvider = presenceProvider.provider;
 
-    this.presenceProvider.onNotified((e: any) => {
+    this.presenceProvider.onNotified(async (e: any) => {
       if (e.type === Methods.NotifyMessageReceivedName) {
-        const message = e.body;
-        if (this.messageHandlers.has(message.type)) {
-          this.messageHandlers.get(message.type)!(message.sender, message.body);
+        const { body, type } = e.body;
+        if (this.messageHandlers.has(type)) {
+          const sender = body.sender;
+          delete body.sender;
+
+          await this.messageHandlers.get(type)!(sender, body);
         }
       }
     });
@@ -49,24 +52,26 @@ export class ContactMessageManager {
   public registerMessageHandler(type: string, callback: Function) {
     this.messageHandlers.set(type, callback);
 
-    return (targetContactId: string, body: any = {}) => {
-      this.sendMessage(targetContactId, type, body);
+    return async (email: string, body: any = {}) => {
+      await this.sendMessage(email, type, body);
     };
   }
 
-  public async sendMessage(
-    targetContactId: string,
-    type: string,
-    body: any = {}
-  ) {
-    const message = {
-      type,
-      body,
-      targetContactId,
-      sender: {
+  public async sendMessage(email: string, type: string, body: any = {}) {
+    Object.defineProperty(body, "sender", {
+      value: {
         displayName: this.vsls.session.user!.displayName,
         email: this.vsls.session.user!.emailAddress
       }
+    });
+
+    const { contacts } = await this.vsls.getContacts([email]);
+    const targetContactId = contacts[email].id;
+
+    const message = {
+      type,
+      body,
+      targetContactId
     };
 
     await this.presenceProvider.requestAsync(
--- src/contacts/messaging/joinRequest.ts
+++ src/contacts/messaging/joinRequest.ts
@@ -10,39 +10,17 @@ export function registerJoinRequest(
   vslsApi: LiveShare,
   messageManager: ContactMessageManager
 ) {
-  const joinRequest = messageManager.registerMessageHandler(
-    JOIN_MESSAGE_TYPE,
-    async (sender: MessageSender, body: any) => {
-      const message = `${sender.displayName} asked to join a collaboration session with you.`;
-      const response = await window.showInformationMessage(message, "Share");
-      if (response) {
-        messageManager.sendMessage(
-          sender.emailAddress,
-          JOIN_ACCEPTED_MESSAGE_TYPE
-        );
-        const { contacts } = await vslsApi.getContacts([sender.emailAddress]);
-        await contacts[sender.emailAddress].invite({ useEmail: false });
-      } else {
-        messageManager.sendMessage(
-          sender.emailAddress,
-          JOIN_REJECTED_MESSAGE_TYPE
-        );
-      }
-    }
-  );
-
   let resolveFunction: Function;
-
-  messageManager.registerMessageHandler(
+  const acceptRequest = messageManager.registerMessageHandler(
     JOIN_ACCEPTED_MESSAGE_TYPE,
-    (sender: MessageSender, body: any) => {
+    async (sender: MessageSender, body: any) => {
       resolveFunction && resolveFunction();
     }
   );
 
-  messageManager.registerMessageHandler(
+  const rejectRequest = messageManager.registerMessageHandler(
     JOIN_REJECTED_MESSAGE_TYPE,
-    (sender: MessageSender, body: any) => {
+    async (sender: MessageSender, body: any) => {
       resolveFunction && resolveFunction();
 
       const message = `${sender.displayName} declined your collaboration request.`;
@@ -50,17 +28,32 @@ export function registerJoinRequest(
     }
   );
 
-  return (name: string, email: string) => {
-    joinRequest(email);
+  const joinRequest = messageManager.registerMessageHandler(
+    JOIN_MESSAGE_TYPE,
+    async (sender: MessageSender, body: any) => {
+      const message = `${sender.displayName} asked to join a collaboration session with you.`;
+      const response = await window.showInformationMessage(message, "Share");
+      if (response) {
+        acceptRequest(sender.emailAddress);
 
+        const { contacts } = await vslsApi.getContacts([sender.emailAddress]);
+        await contacts[sender.emailAddress].invite({ useEmail: false });
+      } else {
+        rejectRequest(sender.emailAddress);
+      }
+    }
+  );
+
+  return async (name: string, email: string) => {
     window.withProgress(
       {
         location: ProgressLocation.Notification,
         title: `Waiting for ${name} to respond...`
       },
       () => {
-        return new Promise(resolve => {
+        return new Promise(async resolve => {
           resolveFunction = resolve;
+          await joinRequest(email);
         });
       }
     );
